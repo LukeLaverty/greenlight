25c2ce7fdf8fa348e79a771e6449d65ea6706bca
Luke Laverty <luke@lukelaverty.co.uk>
2025-10-13 22:11:30 +0100

    Initial Commit

---
+++ Greenlight/GreenlightApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreenlightApp.swift
+//  Greenlight
+//
+//  Created by Luke Laverty on 13/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct GreenlightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
