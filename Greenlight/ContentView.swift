8bb48349ed484e8be2e255c2b462dd70cc3638ba
Luke Laverty <luke@lukelaverty.co.uk>
2025-10-14 23:22:13 +0100

    Implements Tinder-style swipe

--- Greenlight/ContentView.swift
+++ Greenlight/ContentView.swift
@@ -1,34 +1,58 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var green = generateGreen()
+    @State private var green: Color = generateGreen()
+    @State private var offset: CGSize = .zero
 
     var body: some View {
         ZStack {
-            green.ignoresSafeArea()
-
-            VStack(spacing: 40) {
+            Color.gray.ignoresSafeArea()
+   
+            VStack(spacing: 20) {
                 Text("Greenlight?")
-                    .font(.title)
-                    .foregroundColor(.white)
-
-                HStack(spacing: 50) {
-                    Button(action: updateGreen) {
-                        Image(systemName: "hand.thumbsdown.fill")
-                            .font(.system(size: 40))
-                            .foregroundColor(.white)
-                    }
-
-                    Button(action: updateGreen) {
-                        Image(systemName: "hand.thumbsup.fill")
-                            .font(.system(size: 40))
-                            .foregroundColor(.white)
-                    }
-                }
+                    .font(.largeTitle.bold().italic())
+                    .foregroundColor(Color.white)
                 
-                Text(green.description.prefix(7))
-                    .font(.title)
-                    .foregroundColor(.white)
+                GeometryReader { geometry in
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 30)
+                            .fill(Color.white)
+                        
+                        VStack(spacing: 0) {
+                            RoundedRectangle(cornerRadius: 20)
+                                .fill(green)
+                                .padding(EdgeInsets(top: 10, leading: 10, bottom: 0, trailing: 10))
+                                
+                            Text(green.description.prefix(7))
+                                .font(.title)
+                                .padding(5)
+                        }
+                        
+                    }
+                    .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20))
+                    .frame(width: geometry.size.width, height: geometry.size.height)
+                    .offset(x: offset.width, y: 0)
+                    .rotationEffect(.degrees(Double(offset.width / 50)))
+                    .gesture(DragGesture()
+                        .onChanged { gesture in
+                            offset = gesture.translation
+                        }
+                        .onEnded { _ in
+                            if abs(offset.width) > geometry.size.width / 2 {
+                                updateGreen()
+                                // TODO: Hide card and show new card underneath instead.
+                                withAnimation {
+                                    offset = .zero
+                                }
+                            } else {
+                                withAnimation {
+                                    offset = .zero
+                                }
+                            }
+                        }
+                    )
+                    .animation(.spring(), value: offset)
+                }
             }
         }
     }
