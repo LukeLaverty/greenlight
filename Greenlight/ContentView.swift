2d934ce5619d226cbceaffbf1b2259c15728d2a3
Luke Laverty <luke@lukelaverty.co.uk>
2025-10-16 21:40:26 +0100

    Adds initial bottom tab view

--- Greenlight/ContentView.swift
+++ Greenlight/ContentView.swift
@@ -2,26 +2,50 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            Color.gray.ignoresSafeArea()
-   
-            VStack(spacing: 20) {
-                Text("Greenlight?")
-                    .font(.largeTitle.bold().italic())
-                    .foregroundColor(Color.white)
-            
-                ZStack {
-                    SwipeableCard()
-                    SwipeableCard()
-                    SwipeableCard()
-                    SwipeableCard()
-                    SwipeableCard()
-                    SwipeableCard()
-                    SwipeableCard()
-                    SwipeableCard()
-                    SwipeableCard()
-                    SwipeableCard()
+        TabView {
+            ZStack {
+                Color.gray.ignoresSafeArea()
+                
+                VStack(spacing: 20) {
+                    Text("Greenlight?")
+                        .font(.largeTitle.bold().italic())
+                        .foregroundColor(Color.white)
+                    
+                    ZStack {
+                        SwipeableCard()
+                        SwipeableCard()
+                        SwipeableCard()
+                        SwipeableCard()
+                        SwipeableCard()
+                        SwipeableCard()
+                        SwipeableCard()
+                        SwipeableCard()
+                        SwipeableCard()
+                        SwipeableCard()
+                    }
                 }
+                .padding(EdgeInsets(top: 0, leading: 0, bottom: 20, trailing: 0))
+            }
+            .tabItem {
+                Label("Home", systemImage: "arrowshape.left.arrowshape.right")
+            }
+            
+            ZStack {
+                
+            }.tabItem {
+                Label("Liked", systemImage: "heart")
+            }
+            
+            ZStack {
+                
+            }.tabItem {
+                Label("Statistics", systemImage: "chart.xyaxis.line")
+            }
+            
+            ZStack {
+                
+            }.tabItem {
+                Label("Settings", systemImage: "gear")
             }
         }
     }
